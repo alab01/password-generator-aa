9403a1ed5ad85d29da027a957a58152d1a98959e
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-05-31 09:41:52 -0700

    added prompts and variables for all options into generate password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var special = "!@$=<>?#%";
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -19,11 +20,26 @@ function generatePassword() {
     if (lowercase === "yes") {
       shouldIncludeLowercase = true;
     }
+    
+    var uppercase = window.prompt("Do you want to include a uppercase characters?");
+    var shouldIncludeUppercase = false;
+    if (uppercase === "yes") {
+      shouldIncludeUppercase = true;
+    }
+    
+    var numeric = window.prompt("Do you want to include numeric characters?");
+    var shouldIncludeNumeric = false;
+    if (numeric === "yes") {
+      shouldIncludeNumeric = true;
+    }
+
+    var specialCharacter = window.prompt("Do you want to include special characters?");
+    var shouldIncludeSpecialCharacter = false;
+    if (specialCharacter === "yes") {
+      shouldIncludeSpecialCharacter = true;
+    }
   }
 }
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
